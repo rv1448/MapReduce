c72960324826d6b55b4def0f35df1b087fd94bcf
rahulreddy1558 <rahulreddy1558@gmail.com>
2018-12-13 01:09:47 -0800

    Word count with Tool  interface and Toolrunner class

--- src/main/java/mapreduce/WeblogAnalysisv1.java
+++ src/main/java/mapreduce/WeblogAnalysisv1.java
@@ -1,4 +1,45 @@
 package mapreduce;
 
-public class WeblogAnalysisv1 {
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.conf.Configured;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class WeblogAnalysisv1 extends Configured implements Tool {
+
+
+     public class WeblogAnalysismapper extends Mapper<LongWritable, Text,Text,Text>{
+
+         @Override
+         public  void map(LongWritable key, Text value, Context context ){
+
+
+         }
+     }
+
+
+     public class WeblogAnalysisreducer extends Reducer<Text,Text, Text,Text>{
+         @Override
+         public void reduce(Text a, Iterable<Text> b, Context context){
+
+         }
+
+     }
+
+    public int run(String[] args) throws IOException{
+         Configuration conf = new Configuration();
+         Job job = new Job(conf, "Weblog analyzer");
+
+         job.setJarByClass(WeblogAnalysisv1.class);
+
+
+         return -1;
+    }
 }
