4030e5343c50d842dd38766db02cb68a434559ee
rahulreddy1558 <rahulreddy1558@gmail.com>
2018-12-16 22:42:17 -0800

    salary Aggregation from the data from the file

--- src/main/java/mapreduce/SalaryAggregator.java
+++ src/main/java/mapreduce/SalaryAggregator.java
@@ -1,20 +1,41 @@
 package mapreduce;
 
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import sun.plugin2.jvm.CircularByteBuffer;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.ToolRunner;
 
-public class SalaryAggregator {
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-public static class Salarymapper extends Mapper<LongWritable, Text,Text, IntWritable>{
+public  class SalaryAggregator extends Configured implements Tool {
+
+public  static class Salarymapper extends Mapper<LongWritable, Text,Text, IntWritable>{
+        public IntWritable salary;
+     public String creditcardtype;
+
+    public  String[] k;
+    public Text cardtype;
+
+
     @Override
     public void map(LongWritable Key, Text value, Context context){
+        k = value.toString().split("|");
 
-
+        creditcardtype = k[3];
+        salary.set(Integer.parseInt(k[2]));
+        cardtype.set(k[3]);
+        Context.write(cardtype, salary);
 
     }
 
@@ -33,5 +54,40 @@ public static class SalaryReducer extends Reducer<Text, IntWritable, Text, IntWr
 }
 
 
+    public int run(String[] args) throws IOException,InterruptedException, ClassNotFoundException {
+    if(args.length!= 2){
+        System.err.println("please pass all the parameters needed");
 
+        return -1;
+    }
+
+    Configuration conf = new Configuration();
+    Job job = new Job(conf,"Salary Aggregator");
+
+    job.setJarByClass(SalaryAggregator.class);
+
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    job.setMapperClass(Salarymapper.class);
+    job.setReducerClass(SalaryReducer.class);
+
+    Path input = new Path(args[0]);
+    Path output = new Path(args[1]);
+
+    FileInputFormat.addInputPath(job,input);
+    FileOutputFormat.setOutputPath(job,output);
+
+    return job.waitForCompletion(true)? 0:1;
+
+
+
+}
+
+public static   void main(String[] args) throws Exception{
+    int exitcode = ToolRunner.run(new SalaryAggregator(),args);
+
+    System.exit(exitcode);
+}
 }
