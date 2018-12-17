37d14a01a6e8ff683a478c77e136e443a5c1d04d
rahulreddy1558 <rahulreddy1558@gmail.com>
2018-12-16 21:02:21 -0800

    salary Aggregation from the data from the file

--- src/main/java/mapreduce/SalaryAggregator.java
+++ src/main/java/mapreduce/SalaryAggregator.java
@@ -1,4 +1,37 @@
 package mapreduce;
 
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import sun.plugin2.jvm.CircularByteBuffer;
+
 public class SalaryAggregator {
+
+public static class Salarymapper extends Mapper<LongWritable, Text,Text, IntWritable>{
+    @Override
+    public void map(LongWritable Key, Text value, Context context){
+
+
+
+    }
+
+
+}
+
+public static class SalaryReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+
+    @Override
+    public void reduce(Text Key, Iterable<IntWritable> value, Context context){  // Forgetting Iterable
+
+
+
+    }
+
+}
+
+
+
 }
