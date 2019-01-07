ba3f11fe99c97abd0035b64c17a393d6e2da0e57
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-01-07 00:00:59 -0800

    Test Inverted Index

--- src/main/java/Misc/Inverted_index.java
+++ src/main/java/Misc/Inverted_index.java
@@ -9,19 +9,39 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.ToolRunner;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Inverted_index extends Configured implements Tool
 {
 
     public static class Invertedmapper extends Mapper<LongWritable, Text, Text,Text>{
 
+        private Text word = new Text();
+        private Text filename = new Text();
+
         @Override
-        public void map( LongWritable key, Text value, Context context){
+        public void map( LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            FileSplit currentSplit = (FileSplit)context.getInputSplit();
+            String filenamestr = currentSplit.getPath().getName();
+            filename = new Text(filenamestr);
+
+            String line = value.toString();
+
+            StringTokenizer stringsplit = new StringTokenizer(line);
+
+            while(stringsplit.hasMoreTokens()){
+                word.set(stringsplit.nextToken());
+                context.write(word,filename);
+            }
+
 
         }
 
@@ -32,6 +52,19 @@ public class Inverted_index extends Configured implements Tool
         @Override
         public void reduce(Text Key, Iterable<Text> value, Context context){
 
+            StringBuilder stringbuilder = new StringBuilder();
+
+            for (Text val: value){
+
+                stringbuilder.append(val.toString());
+
+                if(value.iterator().hasNext()){
+                    stringbuilder.append("|");
+                }
+
+            }
+
+            context.t
         }
 
     }
@@ -52,6 +85,12 @@ public class Inverted_index extends Configured implements Tool
 
         job.setMapperClass(Invertedmapper.class);
         job.setReducerClass(Invertedreducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
 
         Path input = new Path(args[0]);
         Path output = new Path(args[1]);
