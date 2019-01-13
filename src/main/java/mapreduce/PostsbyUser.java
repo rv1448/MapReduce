3d4ba3f49ca60bafdbf50b7811d0662ccf92dee5
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-01-13 04:16:10 -0800

    stackoverflow data analysis

--- src/main/java/mapreduce/PostsbyUser.java
+++ src/main/java/mapreduce/PostsbyUser.java
@@ -10,26 +10,35 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.Tool;
 
+import java.io.IOException;
+
 public class PostsbyUser extends Configured implements Tool {
 
     public static class DataMapper extends  Mapper<LongWritable, Text,Text, IntWritable>{
 
-    @Override
-    public void map(IntWritable key, Text value, Context context){
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+           IntWritable count = new IntWritable(1);
 
-    }
-
-
+           String[] a = value.toString().split(",");
+           Text word = new Text(a[1]);
+           context.write(word, count);
+        }
     }
 
 
     public static class DataReducer extends Reducer<Text, IntWritable,Text, IntWritable>{
+        @Override
+        public void reduce(Text a, Iterable<IntWritable> b, Context context) throws IOException, InterruptedException{
+            int sum = 0 ;
+            for(IntWritable x : b){
+                sum += x.get();
 
+            }
+            context.write(a,new IntWritable(sum));
+        }
 
     }
-
-
-
 
     public int run(String[] args){
         return  -1;
