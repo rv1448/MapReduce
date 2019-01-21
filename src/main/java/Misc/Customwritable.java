95c001426d34fe9603505e1823e4a872ce1cf837
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-01-21 00:04:38 -0800

    Custom writable test

--- src/main/java/Misc/Customwritable.java
+++ src/main/java/Misc/Customwritable.java
@@ -12,6 +12,8 @@ public class Customwritable {
             }
         }
 
+
+
         public static void main(String[] args)
         {
             System.out.println("*** Primitive Writables ***") ;
